8d7907e2b44ebd381998e1461b9fd00b55c79752
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-20 13:34:18 -0400

    completed new exercises for chapter 2

---
+++ chapter_2/2019_aug_20.py
@@ -0,0 +1,10 @@
+# 2-1: simple message
+message_one = "This is the first message."
+print(message_one)
+
+# 2-2: simple messages
+message_two = "This is the second message."
+print(message_two)
+
+message_two = "We reassigned the value of message_two to be this message."
+print(message_two)
