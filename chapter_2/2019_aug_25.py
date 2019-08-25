4031208f3a9bf2f0b4190c1101e4d06d960bdf97
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-25 17:28:30 -0400

    completed exercise. lower, upper, & title cases

--- chapter_2/2019_aug_25.py
+++ chapter_2/2019_aug_25.py
@@ -1,4 +1,9 @@
 # 2-3: personal message
-
 name = 'susan';
-print(f"{name}")
+print(f"{name}");
+
+# 2-4: name cases
+name = 'james';
+print(f"{name.lower()}");
+print(f"{name.upper()}");
+print(f"{name.title()}");
