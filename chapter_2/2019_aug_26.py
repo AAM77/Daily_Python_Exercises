bad8a1489da2a79eb9db167d3c39602096c5071a
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-26 15:15:52 -0400

    completed an exercise.

--- chapter_2/2019_aug_26.py
+++ chapter_2/2019_aug_26.py
@@ -12,5 +12,11 @@ favorite_number = 7;
 message = f"My favorite number is {favorite_number}.";
 print(message);
 
-# 2-10
+# 2-10: Commenting
 # Added basic comments to two of my programns (2-8 & 2-9).
+
+# 3-1: Names
+names = ['person_1', 'person_2', 'person_3'];
+print(names[0]);
+print(names[1]);
+print(names[2]);
