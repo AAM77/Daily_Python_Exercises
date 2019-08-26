65d08f20e5aa28454bd9d7f767bba681c5e493da
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-26 15:16:37 -0400

    completed an exercise.

--- chapter_2/2019_aug_26.py
+++ chapter_2/2019_aug_26.py
@@ -5,6 +5,7 @@ print(10-2);
 print(2*4);
 print(32/4);
 
+
 # 2-9: favorite number
 # outputs a message stored in a variable, that is an f-string, which called a
 # variable that holds my "favorite" number.
@@ -12,8 +13,10 @@ favorite_number = 7;
 message = f"My favorite number is {favorite_number}.";
 print(message);
 
+
 # 2-10: Commenting
 # Added basic comments to two of my programns (2-8 & 2-9).
+
 
 # 3-1: Names
 names = ['person_1', 'person_2', 'person_3'];
@@ -21,11 +24,17 @@ print(names[0]);
 print(names[1]);
 print(names[2]);
 
-# 3-2: Greetings
 
+# 3-2: Greetings
 message = f"Hi {names[0]}! You're cool.";
 print(message);
 message = f"Hi {names[1]}! You're cool.";
 print(message);
 message = f"Hi {names[2]}! You're cool.";
 print(message);
+
+
+# 3-3: Your own list.
+vehicles = ['car', 'boat'];
+print(f"I like to drive a {vehicles[0]}.");
+print(f"I want to own a {vehicles[1]}.");
