de3339fc0de34cd426263b71746a1b4e9a09d29e
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-26 12:35:30 -0400

    completed an exercise (regarding adding comments to code).

--- chapter_2/2019_aug_26.py
+++ chapter_2/2019_aug_26.py
@@ -1,10 +1,16 @@
 # 2-8: number eight
+# outputs the result of mathematical expressions that all equal to the number 8.
 print(2+6);
 print(10-2);
 print(2*4);
 print(32/4);
 
 # 2-9: favorite number
+# outputs a message stored in a variable, that is an f-string, which called a
+# variable that holds my "favorite" number.
 favorite_number = 7;
 message = f"My favorite number is {favorite_number}.";
 print(message);
+
+# 2-10
+# Added basic comments to two of my programns (2-8 & 2-9).
