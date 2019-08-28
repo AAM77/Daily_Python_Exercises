349247fb5614000ab4505068de749ab3d00d8413
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-27 23:02:30 -0400

    moved chapter 3 exercises into a different folder.

---
+++ chapter_3/2019_aug_26.py
@@ -0,0 +1,20 @@
+# 3-1: Names
+names = ['person_1', 'person_2', 'person_3'];
+print(names[0]);
+print(names[1]);
+print(names[2]);
+
+
+# 3-2: Greetings
+message = f"Hi {names[0]}! You're cool.";
+print(message);
+message = f"Hi {names[1]}! You're cool.";
+print(message);
+message = f"Hi {names[2]}! You're cool.";
+print(message);
+
+
+# 3-3: Your own list.
+vehicles = ['car', 'boat'];
+print(f"I like to drive a {vehicles[0]}.");
+print(f"I want to own a {vehicles[1]}.");
