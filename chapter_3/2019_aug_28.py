ae41e627f62168836eed6d1a96c30e063486e3fa
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-28 11:50:17 -0400

    completed a new exercise.

--- chapter_3/2019_aug_28.py
+++ chapter_3/2019_aug_28.py
@@ -26,4 +26,28 @@ print(f"original cars list: {cars}\n")
 # finding the length using '  len( [list] )  '
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 len(cars)
-print(f"length of the list / # items in list: {len(cars)}")
+print(f"length of the list / # items in list: {len(cars)}\n\n\n\n\n")
+
+# EXERCISES
+
+# 3-8: seeing the world (use sorted(), sorted(list, reverse=True), reverse(), sort(), & sort(reverse=True))
+places = ['new zealand', 'pakistan', 'mecca', 'madina', 'alaska', 'australia', 'switzerland', 'canada']
+print(f"original: {places}\n")
+
+print(f"w/ sorted(): {sorted(places)}")
+print(f"current list order: {places}\n")
+
+print(f"w/ sorted() & reverse=True: {sorted(places, reverse=True)}")
+print(f"current list order: {places}\n")
+
+places.reverse()
+print(f"current list (after x1 reverse()): {places}\n")
+
+places.reverse()
+print(f"current list (after x2 reverse()): {places}\n")
+
+places.sort()
+print(f"current list (after sort()): {places}\n")
+
+places.sort(reverse=True)
+print(f"current list (after sort(reverse=True)): {places}\n")
