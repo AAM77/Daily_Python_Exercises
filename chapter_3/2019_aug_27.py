f09364905f99d8bd546f23799c03b655fc83718a
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-27 23:24:32 -0400

    added a new exercise.

--- chapter_3/2019_aug_27.py
+++ chapter_3/2019_aug_27.py
@@ -3,3 +3,10 @@ guests = ['person1', 'person2', 'person3']
 for guest in guests:
     print("Hello {g}".format(g=guest)) # works in all python versions
     print(f'Hello {guest}.') # works only in python 3.6+
+
+
+
+# 3-5 changing guest list
+guests[2] = 'new_person3'
+for guest in guests:
+    print(f"Hello {guest}.")
